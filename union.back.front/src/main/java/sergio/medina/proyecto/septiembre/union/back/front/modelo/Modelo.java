6349a205ecb1a5b22b22e0e835fd3db33c9559f3
Sergio <sermedproject@gmail.com>
2024-09-20 10:14:38 +0200

    Actualizado Readme

--- union.back.front/src/main/java/sergio/medina/proyecto/septiembre/union/back/front/modelo/Modelo.java
+++ union.back.front/src/main/java/sergio/medina/proyecto/septiembre/union/back/front/modelo/Modelo.java
@@ -1,14 +1,21 @@
+// Leyenda:
+// BD = Base de datos
+// M = Modelo.java
+// I = Interfaz.java
+// S = Servicios.java
+// C = Controlador.java
+
 package sergio.medina.proyecto.septiembre.union.back.front.modelo;
-
-
 import jakarta.persistence.*;
+
+//1º Creo el modelo (esta clase) que tomará datos de la BD por lo que debe indicarse datos correctamente.
 
 @Entity
 @Table (name="poderosos")
 public class Modelo {
-
+    //---Variables
     @Id
-    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    @GeneratedValue(strategy= GenerationType.IDENTITY) //Investigar acerca de Identity y otras posibiidades
     @Column(name="id")
     private int id;
 
@@ -17,45 +24,51 @@ public class Modelo {
 
     @Column(name="ojos")
     private int ojos;
+    //---Variables
 
+    //---Constructores
     public Modelo(){
-//        Es necesario este constructor vacío para que
-//        pueda mostrarse el GET de muestralo() en Controlador
     }
+    //↑ Necesario constructor vacío para poder mostrar el GET de muestralo() en C.
 
+    public Modelo(String nombre, int ojos){
+        this.nombre = nombre;
+        this.ojos = ojos;
+    }
+    //↑ Constructor con 2 parámetros (nombre, ojos)
+
+    //---Constructores
+
+    //---Métodos
     @Override
     public String toString(){
         return "\nMonstruo número: " + this.getId() +
                 "\nNombre: " + this.getNombre() +
                 "\nCantidad de ojos: " + this.getOjos();
     }
+    //↑ Muestra este formato cuando se llama a mostrar() de S.
 
-    public Modelo(String nombre, int ojos){
-        this.nombre = nombre;
-        this.ojos = ojos;
-    }
+    //---Métodos
 
+    //---Setter y Getters
     public int getId() {
         return id;
     }
-
     public void setId(int id) {
         this.id = id;
     }
-
     public String getNombre() {
         return nombre;
     }
-
     public void setNombre(String nombre) {
         this.nombre = nombre;
     }
-
     public int getOjos() {
         return ojos;
     }
-
     public void setOjos(int ojos) {
         this.ojos = ojos;
     }
+
+    //---Setter y Getters
 }
