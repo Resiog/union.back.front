a23df6f46acd863a8abd34d6efbf4d0636461393
Sergio <sermedproject@gmail.com>
2024-09-20 10:44:38 +0200

    Actualizado comentarios

--- union.back.front/src/main/java/sergio/medina/proyecto/septiembre/union/back/front/controlador/Controlador.java
+++ union.back.front/src/main/java/sergio/medina/proyecto/septiembre/union/back/front/controlador/Controlador.java
@@ -4,11 +4,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import sergio.medina.proyecto.septiembre.union.back.front.modelo.Modelo;
 import sergio.medina.proyecto.septiembre.union.back.front.servicios.Servicios;
-
 import java.util.List;
 
 @RestController
-@RequestMapping("/listado")
+@RequestMapping("/listado") //Debe ingresarse a localhost:8080/listado
 public class Controlador {
 
     @Autowired
@@ -18,29 +17,39 @@ public class Controlador {
 //    public List<Modelo> muestralo(){
 //        return servicio.mostrar();
 //    }
+//    Método comentado para obtener listado en formato List<Modelo>
 
     @GetMapping
     public String muestralo(){
         return servicio.mostrar();
     }
+    //↑ Método GET, accede a método mostrar() de Servicios.java y retorna resultado, mostrando un String en pantalla.
 
     @PostMapping
     public Modelo insertarCriatura (@RequestBody Modelo kami){
         return servicio.insertar(kami);
     }
+    //↑ Método POST, accede a insertar(Modelo insertarKami) de Servicios.java. Retorna una clase Modelo
+    // con método .save lo que hace guardar lo insertado en el listado de la BD.
 
     @PutMapping
     public Modelo actualizarCriatura(@RequestBody Modelo kami){ //Entra un modelo de Kami
         return servicio.actualizar(kami); //Retorna la función ejecutada en Servicios tomando kami como parámetro
     }
+    //↑ Método PUT, accede a actualizar(Modelo actualizarKami) de Servicios.java. Retorna una clase Modelo
+    // con método .save si encuentra registro a partir de ID, actualizando lo insertado en BD.
 
     @GetMapping("/{id}")
     public String compruebaId(@PathVariable int id){
         return servicio.comprobarSiId(id);
     }
+    //↑ Método GET, accede a comprobarSiId(int id) de Servicios.java. Retorna una clase String informando del
+    // nombre del kami relacionado con el id insertado.
 
     @DeleteMapping("/{id}")
     public List<Modelo> borrar(@PathVariable int id){
         return servicio.borrar(id);
     }
+    //↑ Método DELETE, accede a borrar(int id) de Servicios.java. Retorna una lista con la actualización de
+    // la BD a efectos de comprobar que se ha borrado el kami elegido mediante el ID.
 }
--- union.back.front/src/main/java/sergio/medina/proyecto/septiembre/union/back/front/servicios/Servicios.java
+++ union.back.front/src/main/java/sergio/medina/proyecto/septiembre/union/back/front/servicios/Servicios.java
@@ -4,9 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import sergio.medina.proyecto.septiembre.union.back.front.interfaz.Interfaz;
 import sergio.medina.proyecto.septiembre.union.back.front.modelo.Modelo;
-
 import java.util.List;
-import java.util.Optional;
 
 @Service
 public class Servicios {
@@ -17,19 +15,23 @@ public class Servicios {
 //    public List<Modelo> mostrar(){
 //        return interfaz.findAll();
 //    }
+//    ↑ Método para retornar un List<Modelo> en vez de un String
 
     public String mostrar(){
         return interfaz.findAll().toString();
     }
+    //↑ Muestra listado, relacionado con GET de C.
 
     public Modelo insertar(Modelo insertarKami){
         return interfaz.save(insertarKami);
     }
+    //↑ Permite insertar nuevos Kamis (seres sobrenaturales de la mitología japonesa), relacionado con POST de C.
 
     public String comprobarSiId(int id){
         Modelo kamiBuscado = interfaz.findById(id).orElse(null);
         return kamiBuscado.getNombre();
     }
+    //↑ Permite obtener el nombre de un Kami mediante búsqueda por ID.
 
     public Modelo actualizar(Modelo actualizarKami){ //Entra un modelo de Kami y retorna un Modelo
         int id = actualizarKami.getId(); //Se obtiene el id del modelo que entra
@@ -40,6 +42,7 @@ public class Servicios {
         }
         return interfaz.save(aCambiar); //Retorna guardando el modelo encontrado por ID y que fue cambiado
     }
+    //↑ Permite actualizar cualquier Kami mediante introducción id + datos. Se relaciona con UPDATE de C.
 
     public List<Modelo> borrar(int id){
         if (interfaz.existsById(id) == true){
@@ -49,4 +52,5 @@ public class Servicios {
         }
         return interfaz.findAll();
     }
+    //↑ Permite eliminar cualquier kami mediante introducción de su id. Se relaciona con DELETE de C.
 }
