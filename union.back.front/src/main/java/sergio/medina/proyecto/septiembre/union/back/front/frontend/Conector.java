c4c2f66ce40be506171681b456b2a2b40d78b617
Sergio <sermedproject@gmail.com>
2024-09-21 09:58:57 +0200

    probado conector BD

---
+++ union.back.front/src/main/java/sergio/medina/proyecto/septiembre/union/back/front/frontend/Conector.java
@@ -0,0 +1,17 @@
+package sergio.medina.proyecto.septiembre.union.back.front.frontend;
+
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class Conector {
+    public HttpClient cliente;
+    public HttpRequest request;
+    public HttpResponse <String> respuesta;
+
+    public Conector(){
+
+    }
+
+
+}
--- union.back.front/src/main/java/sergio/medina/proyecto/septiembre/union/back/front/frontend/UI.java
+++ union.back.front/src/main/java/sergio/medina/proyecto/septiembre/union/back/front/frontend/UI.java
@@ -1,6 +1,13 @@
 package sergio.medina.proyecto.septiembre.union.back.front.frontend;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 
 public class UI extends JFrame{
     private JPanel panel;
@@ -21,6 +28,28 @@ public class UI extends JFrame{
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setSize(450,300);
         setVisible(true);
+
+        btConectar.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                Conector conexion = new Conector();
+                conexion.cliente = HttpClient.newHttpClient();
+                conexion.request = HttpRequest.newBuilder()
+                        .uri(URI.create("http://localhost:8080/listado"))
+                        .build();
+                try {
+                    conexion.respuesta = conexion.cliente.send(conexion.request, HttpResponse.BodyHandlers.ofString());
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                } catch (InterruptedException ex) {
+                    throw new RuntimeException(ex);
+                }
+                System.out.println(conexion.respuesta.statusCode());
+                System.out.println(conexion.respuesta.body());
+            }
+        });
+
+
     }
 
     public static void main(String[] args) {
